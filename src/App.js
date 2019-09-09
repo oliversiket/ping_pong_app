1d440bee1ab336d3582d0de953775823b7bf4d21
Ollie <oliver.siket@gmail.com>
2019-09-09 12:50:16 +0100

    app displaying initial scores

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ( { player1score, player2score, handleIncrement } ) => (
+    <React.Fragment>
+        {/* header */}
+        <header className="page-header">
+            <h1>PongPing</h1>
+        </header>
+
+        {/* scores */}
+        <div className="row">
+            <div className="col-xs-6">
+                <p>Player 1 <span className="pull-right label label-success">Serving</span></p>
+                <p className="well">{ player1score }</p>
+                <button 
+                  className="btn btn-primary"
+                  onClick = { handleIncrement }
+                >+</button>
+            </div>
+
+            <div className="col-xs-6">
+                <p>Player 2 <span className="pull-right label label-success">Serving</span></p>
+                <p className="well">{ player2score }</p>
+                <button 
+                  className="btn btn-primary"
+                >+</button>
+            </div>
+        </div>
+
+        { /* winner message */}
+        <h2 className="jumbotron">Player {/* winning player here */} wins!</h2>
+
+        <hr />
+
+        { /* reset button */}
+        <button className="btn btn-danger">Reset</button>
+    </React.Fragment>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,49 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initial = {
+    player1: 0,
+    player2: 0,
+};
+
+let reducer = (state,action) => {
+
+    switch (action.type) {
+        case "incrementp1" : return { ...state, player1: state.player1 + 1 };
+        default: return state;
+    }
+
+};
+
+const store = createStore(
+    reducer,
+    initial,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()    
+);
+
+const render = () => {
+    let state = store.getState();
+    console.log(state.player1);
+    // pass in state.value as a value prop
+    ReactDOM.render(
+        <App 
+            player1score = { state.player1 }
+            player2score = { state.player2 }
+            handleIncrement={ () => store.dispatch({ type: "incrementp1" }) }
+        />,
+    document.getElementById("root")
+    );
+
+};
+
+store.subscribe(render);
+render();
+
+// store.dispatch({ type: "increment" });
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
